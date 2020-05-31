83fe913b256f2f79f6e5c2fd5618ab2a86df8f10
Abdullah-Sheehan <abdshe2005@gmail.com>
2020-05-31 23:15:00 +0600

    initial commit

---
+++ js/opacityonhover.js
@@ -0,0 +1,24 @@
+function o1() {
+    document.getElementById("b2").style.opacity = 0.1;
+    document.getElementById("b3").style.opacity = 0.1;
+}
+function o2() {
+    document.getElementById("b1").style.opacity = 0.1;
+    document.getElementById("b3").style.opacity = 0.1;
+}
+function o3() {
+    document.getElementById("b1").style.opacity = 0.1;
+    document.getElementById("b2").style.opacity = 0.1;
+}
+function o4() {
+    document.getElementById("b2").style.opacity = 1;
+    document.getElementById("b3").style.opacity = 1;
+}
+function o5() {
+    document.getElementById("b1").style.opacity = 1;
+    document.getElementById("b3").style.opacity = 1;
+}
+function o6() {
+    document.getElementById("b1").style.opacity = 1;
+    document.getElementById("b2").style.opacity = 1;
+}
---
+++ js/triggerscroll.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+            $('.html').waypoint(function () {
+                $('.html').css({
+                    animation: "filling 4s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.css').waypoint(function () {
+                $('.css').css({
+                    animation: "filling 4s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.js').waypoint(function () {
+                $('.js').css({
+                    animation: "filling 3s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.react').waypoint(function () {
+                $('.react').css({
+                    animation: "filling 2s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.bootstrap').waypoint(function () {
+                $('.bootstrap').css({
+                    animation: "filling 4s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.jquery').waypoint(function () {
+                $('.jquery').css({
+                    animation: "filling 2.5s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.php').waypoint(function () {
+                $('.php').css({
+                    animation: "filling 3.5s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.wp').waypoint(function () {
+                $('.wp').css({
+                    animation: "filling 3s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.python').waypoint(function () {
+                $('.python').css({
+                    animation: "filling 4s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+            $('.psd').waypoint(function () {
+                $('.psd').css({
+                    animation: "filling 3s",
+                    opacity: "1"
+                });
+            }, {
+                offset: '100%'
+            });
+});
